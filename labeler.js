363741ddb158e6a7c958d3d19bea69b90931f172
dronperminov <dronperminov@gmail.com>
2020-01-23 12:38:37 +0300

    convert labeler to class

--- labeler.js
+++ labeler.js
@@ -1,33 +1,35 @@
-const diff = 5
+function Labeler(labels, colors) {
+	this.diff = 5
 
-var startPoint = null
-var endPoint = null
-var currBox = null
-var entities = []
-var entities_boxes = []
+	this.labels = labels
+	this.colors = colors
 
-var moveIndex = -1
-var movePoint = null
+	this.startPoint = null
+	this.endPoint = null
+	this.currBox = null
+	this.entities = []
+	this.entities_boxes = []
 
-var resizeIndex = -1
-var resizeType = null
-var resizePoint = null
-var borders = [ "left", "top", "right", "bottom"]
+	this.moveIndex = -1
+	this.movePoint = null
 
-var isBlocked = false
+	this.resizeIndex = -1
+	this.resizeType = null
+	this.resizePoint = null
 
-var labels = [ "text", "table", "picture" ]
-var colors = [ "255, 0, 0", "0, 255, 0", "0, 0, 255" ]
-var img = $('.labeler-image')
+	this.isBlocked = false
 
-var offsetLeft = img.offset().left
-var offsetTop = img.offset().top
+	this.img = $('.labeler-image')
 
-function get_point(e) {
-	return { x: e.pageX - offsetLeft, y: e.pageY - offsetTop }
+	this.offsetLeft = this.img.offset().left
+	this.offsetTop = this.img.offset().top
 }
 
-function get_box(startPoint, endPoint, label = "") {
+Labeler.prototype.get_point = function(e) {
+	return { x: e.pageX - this.offsetLeft, y: e.pageY - this.offsetTop }
+}
+
+Labeler.prototype.get_box = function(startPoint, endPoint, label = "") {
 	x = Math.min(startPoint.x, endPoint.x)
 	y = Math.min(startPoint.y, endPoint.y)
 
@@ -43,49 +45,48 @@ function get_box(startPoint, endPoint, label = "") {
 	}
 }
 
-function get_box_index(x, y) {
-	for (let i = 0; i < entities.length; i++) {
-		let box = entities[i]
+Labeler.prototype.get_box_index = function(x, y) {
+	for (let i = 0; i < this.entities.length; i++) {
+		let box = this.entities[i]
 
-		if (x >= box.x + diff && y >= box.y + diff && x <= box.x + box.width - diff && y <= box.y + box.height - diff)
+		if (x >= box.x + this.diff && y >= box.y + this.diff && x <= box.x + box.width - this.diff && y <= box.y + box.height - this.diff)
 			return i
 	}
 
 	return -1
 }
 
-function check_line(x, y, x1, y1, x2, y2) {
-	if (x1 == x2) {
-		return x > x1 - diff && x < x1 + diff && y >= y1 && y <= y2
-	}
-	else if (y1 == y2) {
-		return y > y1 - diff && y < y1 + diff && x >= x1 && x <= x2
-	}
+Labeler.prototype.check_line = function(x, y, x1, y1, x2, y2) {
+	if (x1 == x2)
+		return x > x1 - this.diff && x < x1 + this.diff && y >= y1 && y <= y2
+
+	if (y1 == y2)
+		return y > y1 - this.diff && y < y1 + this.diff && x >= x1 && x <= x2
 
 	return false
 }
 
-function get_box_resize_index(x, y) {
-	for (let i = 0; i < entities.length; i++) {
-		let box = entities[i]
+Labeler.prototype.get_box_resize_index = function(x, y) {
+	for (let i = 0; i < this.entities.length; i++) {
+		let box = this.entities[i]
 
-		if (check_line(x, y, box.x, box.y, box.x, box.y + box.height))
+		if (this.check_line(x, y, box.x, box.y, box.x, box.y + box.height))
 			return { index: i, type: 1 }
 			
-		if (check_line(x, y, box.x, box.y, box.x + box.width, box.y))
+		if (this.check_line(x, y, box.x, box.y, box.x + box.width, box.y))
 			return { index: i, type: 2 }
 
-		if (check_line(x, y, box.x + box.width, box.y, box.x + box.width, box.y + box.height))
+		if (this.check_line(x, y, box.x + box.width, box.y, box.x + box.width, box.y + box.height))
 			return { index: i, type: 3 }
 			
-		if (check_line(x, y, box.x, box.y + box.height, box.x + box.width, box.y + box.height))
+		if (this.check_line(x, y, box.x, box.y + box.height, box.x + box.width, box.y + box.height))
 			return { index: i, type: 4 }
 	}
 
 	return null
 }
 
-function show_entities() {
+Labeler.prototype.show_entities = function() {
 	let image = $('.labeler-image img')
 	let name = image.attr("src")
 	let index = name.lastIndexOf('/')
@@ -98,104 +99,106 @@ function show_entities() {
 
 	let tmp = []
 
-	for (let i = 0; i < entities.length; i++) {
+	for (let i = 0; i < this.entities.length; i++) {
 		tmp.push({
-			label: entities[i].label,
-			x: Math.max(0, entities[i].x / width),
-			y: Math.max(0, entities[i].y / height),
-			width: Math.min(entities[i].width / width, 1),
-			height: Math.min(entities[i].height / height, 1),
+			label: this.entities[i].label,
+			x: Math.max(0, this.entities[i].x / width),
+			y: Math.max(0, this.entities[i].y / height),
+			width: Math.min(this.entities[i].width / width, 1),
+			height: Math.min(this.entities[i].height / height, 1),
 		})
 	}
 
 	$("#entities-data").text(JSON.stringify({ name: name, entities: tmp }, null, "  "));
 }
 
-function get_color(label, opacity = false) {
-	return "rgba(" + colors[labels.indexOf(label)] + (opacity ? ", 0.15" : "") + ")"
+Labeler.prototype.get_color = function(label, opacity = false) {
+	return "rgba(" + this.colors[this.labels.indexOf(label)] + (opacity ? ", 0.15" : "") + ")"
 }
 
-function boxes_hover(p) {
-	let index = get_box_index(p.x, p.y)
+Labeler.prototype.boxes_hover = function(p) {
+	let index = this.get_box_index(p.x, p.y)
 
-	for (let i = 0; i < entities.length; i++)
-		entities_boxes[i].css('outline', "2px solid " + get_color(entities[i].label))
+	for (let i = 0; i < this.entities.length; i++)
+		this.entities_boxes[i].css('outline', "2px solid " + this.get_color(this.entities[i].label))
 
 	if (index != -1) {
-		entities_boxes[index].css('outline', "2px dashed #ffbc00")
-		img.css('cursor', "pointer")
+		this.entities_boxes[index].css('outline', "2px dashed #ffbc00")
+		$("body").css('cursor', "pointer")
 		return
 	}
 
-	img.css('cursor', "default")
+	$("body").css('cursor', "default")
 
-	let resizing = get_box_resize_index(p.x, p.y)
+	let resizing = this.get_box_resize_index(p.x, p.y)
 
 	if (resizing != null) {
+		this.entities_boxes[resizing.index].css('outline', "2px dashed #ffbc00")
+
 		if (resizing.type == 1 || resizing.type == 3)
-			img.css('cursor', "w-resize")
+			$("body").css('cursor', "w-resize")
 		else
-			img.css('cursor', "s-resize")
-		return
+			$("body").css('cursor', "s-resize")
 	}
 }
 
-function start_labeling() {
+Labeler.prototype.start_labeling = function() {
 	let select = $('<select id="label-select"><option>Select label</option><option>' + labels.join("</option><option>") + '</option></select>')
-	select.appendTo(currBox)
+	select.appendTo(this.currBox)
 	select.css({
 		"position" : "absolute",
-		"top" : endPoint.y - Math.min(startPoint.y, endPoint.y) + "px",
-		"left" : endPoint.x - Math.min(startPoint.x, endPoint.x) + "px",
+		"top" : this.endPoint.y - Math.min(this.startPoint.y, this.endPoint.y) + "px",
+		"left" : this.endPoint.x - Math.min(this.startPoint.x, this.endPoint.x) + "px",
 	})
 
 	select.focus()
-	isBlocked = true
+	this.isBlocked = true
+	let labeler = this;
 
 	select.change(function() {
-		end_labeling(select)
+		labeler.end_labeling(select)
 	})
 
 	select.keydown(function(e) {
 		let option = parseInt(e.key)
 
-		if (Number.isInteger(option) && option > 0 && option <= labels.length) {
+		if (Number.isInteger(option) && option > 0 && option <= labeler.labels.length) {
 			select.prop('selectedIndex', option)
-			end_labeling(select)
+			labeler.end_labeling(select)
 		}
 	})
 }
 
-function end_labeling(select) {
+Labeler.prototype.end_labeling = function(select) {
 	let label = select.val()
 
-	currBox.css("background", get_color(label, true))
-	currBox.css("outline", "2px solid " + get_color(label))
+	this.currBox.css("background", this.get_color(label, true))
+	this.currBox.css("outline", "2px solid " + this.get_color(label))
 
 	let text = $("<p>" + label + "</p>")
 	text.css({
 		"position" : "relative",
 		"text-align" : "center",
 		"margin" : "0",
-		"color" : get_color(label)
+		"color" : this.get_color(label)
 	})
 
 	select.remove()
-	text.appendTo(currBox)
+	text.appendTo(this.currBox)
 
-	entities.push(get_box(startPoint, endPoint, label))
-	entities_boxes.push(currBox)
+	this.entities.push(this.get_box(this.startPoint, this.endPoint, label))
+	this.entities_boxes.push(this.currBox)
 
-	startPoint = null
-	endPoint = null
-	isBlocked = false
-	show_entities()
+	this.startPoint = null
+	this.endPoint = null
+	this.isBlocked = false
+	this.show_entities()
 }
 
-function is_valid(p) {
+Labeler.prototype.is_valid = function(p) {
 	let imageWidth = $(".labeler-image img").width()
 	let imageHeight = $(".labeler-image img").height()
-	let dst = moveIndex == -1 && resizeIndex == -1 ? diff : 0;
+	let dst = this.moveIndex == -1 && this.resizeIndex == -1 ? this.diff : 0;
 
 	if (p.x < -dst || p.y < -dst || p.x > imageWidth + dst || p.y > imageHeight + dst)
 		return false
@@ -203,35 +206,46 @@ function is_valid(p) {
 	return true
 }
 
-$(document).mousedown(function(e) {
-	if (isBlocked)
+Labeler.prototype.remove_all = function() {
+	if (this.entities.length > 0 && confirm("Remove all: are you sure?")) {
+		for (let i = 0; i < this.entities_boxes.length; i++)
+			this.entities_boxes[i].remove()
+
+		this.entities = []
+		this.entities_boxes = []
+		this.show_entities()
+	}
+}
+
+Labeler.prototype.mousedown = function(e) {
+	if (this.isBlocked)
 		return
 
-	let p = get_point(e)
+	let p = this.get_point(e)
 
-	if (!is_valid(p))
+	if (!this.is_valid(p))
 		return
 
-	let index = get_box_index(p.x, p.y)
-	let resizing = get_box_resize_index(p.x, p.y)
+	let index = this.get_box_index(p.x, p.y)
+	let resizing = this.get_box_resize_index(p.x, p.y)
 
 	if (e.button == 0) {
 		if (index == -1 && resizing == null) {
-			startPoint = { x: p.x, y: p.y }
-			currBox = $('<div class="label-box" draggable=false></div>')
-			currBox.appendTo(img)
-			moveIndex = -1
+			this.startPoint = { x: p.x, y: p.y }
+			this.currBox = $('<div class="label-box" draggable=false></div>')
+			this.currBox.appendTo(this.img)
+			this.moveIndex = -1
 		}
 		else if (resizing == null) {
-			movePoint = { x: p.x, y: p.y }
-			currBox = entities_boxes[index]
-			moveIndex = index
+			this.movePoint = { x: p.x, y: p.y }
+			this.currBox = this.entities_boxes[index]
+			this.moveIndex = index
 		}
 		else {
-			resizePoint = { x: p.x, y: p.y }
-			currBox = entities_boxes[resizing.index]
-			resizeIndex = resizing.index
-			resizeType = resizing.type
+			this.resizePoint = { x: p.x, y: p.y }
+			this.currBox = this.entities_boxes[resizing.index]
+			this.resizeIndex = resizing.index
+			this.resizeType = resizing.type
 		}
 	}
 	else if (e.button == 2) {
@@ -241,125 +255,122 @@ $(document).mousedown(function(e) {
 		if (index == -1)
 			return
 
-		entities_boxes[index].remove()
-		entities.splice(index, 1)
-		entities_boxes.splice(index, 1)
+		this.entities_boxes[index].remove()
+		this.entities.splice(index, 1)
+		this.entities_boxes.splice(index, 1)
 	}
 
-	show_entities()
-})
+	this.show_entities()
+}
 
-$(document).mouseup(function(e) {
+Labeler.prototype.mouseup = function(e) {
 	if (e.button != 0)
 		return
 
-	if (isBlocked) {
+	if (this.isBlocked) {
 		if (e.target.tagName != "SELECT") {
-			currBox.remove()
-			startPoint = null
-			endPoint = null
-			isBlocked = false
+			this.currBox.remove()
+			this.startPoint = null
+			this.endPoint = null
+			this.isBlocked = false
 		}
 
 		return
 	}
 
-	if (startPoint != null && moveIndex == -1 && resizeIndex == -1) {
-		endPoint = get_point(e)
+	if (this.startPoint != null && this.moveIndex == -1 && this.resizeIndex == -1) {
+		this.endPoint = this.get_point(e)
 
 		let imageWidth = $(".labeler-image img").width()
 		let imageHeight = $(".labeler-image img").height()
 
-		if (endPoint.x > imageWidth)
-			endPoint.x = imageWidth
+		if (this.endPoint.x > imageWidth)
+			this.endPoint.x = imageWidth
 
-		if (endPoint.y > imageHeight)
-			endPoint.y = imageHeight
+		if (this.endPoint.y > imageHeight)
+			this.endPoint.y = imageHeight
 
-		let width = Math.abs(startPoint.x - endPoint.x)
-		let height = Math.abs(startPoint.y - endPoint.y)
+		let width = Math.abs(this.startPoint.x - this.endPoint.x)
+		let height = Math.abs(this.startPoint.y - this.endPoint.y)
 
-		if (width < diff || height < diff) {
-			currBox.remove()
-			startPoint = null
-			endPoint = null
+		if (width < this.diff || height < this.diff) {
+			this.currBox.remove()
+			this.startPoint = null
+			this.endPoint = null
 			return
 		}
 
-		start_labeling()
+		this.start_labeling()
 	}
 
-	moveIndex = -1
-	resizeIndex = -1
-	show_entities()
-})
+	this.moveIndex = -1
+	this.resizeIndex = -1
+	this.show_entities()
+}
 
-$(document).mousemove(function(e) {
-	if (isBlocked)
+Labeler.prototype.mousemove = function(e) {
+	if (this.isBlocked)
 		return
 
-	let p = get_point(e)
+	let p = this.get_point(e)
 
-	if (startPoint == null && moveIndex == -1 && resizeIndex == -1) {
-		boxes_hover(p)
+	if (this.startPoint == null && this.moveIndex == -1 && this.resizeIndex == -1) {
+		this.boxes_hover(p)
 		return
 	}
 
-	if ((moveIndex > -1 || resizeIndex > -1) && !is_valid(p))
+	if ((this.moveIndex > -1 || this.resizeIndex > -1) && !this.is_valid(p))
 		return
 
 	let imageWidth = $(".labeler-image img").width()
 	let imageHeight = $(".labeler-image img").height()
+	let box = null
 
-	if (moveIndex > -1) {
-		let dx = p.x - movePoint.x
-		let dy = p.y - movePoint.y
+	if (this.moveIndex > -1) {
+		let dx = p.x - this.movePoint.x
+		let dy = p.y - this.movePoint.y
 
-		movePoint.x = p.x
-		movePoint.y = p.y
+		this.movePoint.x = p.x
+		this.movePoint.y = p.y
 
-		box = entities[moveIndex]
+		box = this.entities[this.moveIndex]
 		box.x += dx
 		box.y += dy
-
-		show_entities()
 	}
-	else if (resizeIndex > -1) {
-		let dx = p.x - resizePoint.x
-		let dy = p.y - resizePoint.y
+	else if (this.resizeIndex > -1) {
+		let dx = p.x - this.resizePoint.x
+		let dy = p.y - this.resizePoint.y
 
-		resizePoint.x = p.x
-		resizePoint.y = p.y
+		this.resizePoint.x = p.x
+		this.resizePoint.y = p.y
 
-		box = entities[resizeIndex]
+		box = this.entities[this.resizeIndex]
 
-		if (resizeType == 1) {
+		if (this.resizeType == 1) {
 			box.x += dx
 			box.width -= dx
 		}
-		else if (resizeType == 2) {
+		else if (this.resizeType == 2) {
 			box.y += dy
 			box.height -= dy
 		}
-		else if (resizeType == 3) {
+		else if (this.resizeType == 3) {
 			box.width += dx
 		}
-		else if (resizeType == 4) {
+		else if (this.resizeType == 4) {
 			box.height += dy
 		}
 		
 		if (box.height < 1) 
-			resizeType = resizeType == 2 ? 4 : 2;
+			this.resizeType = this.resizeType == 2 ? 4 : 2;
 
 		if (box.width < 1) 
-			resizeType = resizeType == 3 ? 1 : 3;
-
-		show_entities()
+			this.resizeType = this.resizeType == 3 ? 1 : 3;
 	}
 	else {
 		point = { x: p.x, y: p.y }
 
-		if (!is_valid(p)) {
+		if (!this.is_valid(p)) {
 			if (point.x > imageWidth)
 				point.x = imageWidth;
 
@@ -367,13 +378,13 @@ $(document).mousemove(function(e) {
 				point.y = imageHeight;
 		}
 
-		box = get_box(startPoint, point)
+		box = this.get_box(this.startPoint, point)
 	}
 
-	if (box.y < 1)
+	if (box.y < 0)
 		box.y = 0;
 
-	if (box.x < 1)
+	if (box.x < 0)
 		box.x = 0;
 
 	if (box.x + box.width > imageWidth)
@@ -382,7 +393,7 @@ $(document).mousemove(function(e) {
 	if (box.y + box.height > imageHeight)
 		box.y = imageHeight - box.height;
 
-	currBox.css({
+	this.currBox.css({
 		"outline": "2px dotted #ffbc00",
 		"position": "absolute",
 		"top": box.y + "px",
@@ -390,17 +401,19 @@ $(document).mousemove(function(e) {
 		"width": box.width + "px",
 		"height": box.height + "px",
 	})
-})
+}
+
+const labels = [ "text", "table", "picture" ]
+const colors = [ "255, 0, 0", "0, 255, 0", "0, 0, 255" ]
+
+let labeler = new Labeler(labels, colors)
+
+$(document).mousedown(function(e) { labeler.mousedown(e) })
+$(document).mouseup(function(e) { labeler.mouseup(e) })
+$(document).mousemove(function(e) { labeler.mousemove(e) })
+
+labeler.show_entities()
 
 $("#reset-btn").click(function(e) {
-	if (entities.length > 0 && confirm("Remove all: are you sure?")) {
-		for (let i = 0; i < entities_boxes.length; i++)
-			entities_boxes[i].remove()
-
-		entities = []
-		entities_boxes = []
-		show_entities()
-	}
+	labeler.remove_all()
 })
-
-show_entities()
